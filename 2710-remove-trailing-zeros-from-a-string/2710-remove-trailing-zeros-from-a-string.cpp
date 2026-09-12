0d991f16627061bd7117c7266865b27137975c80
NITIN TRIPATHI <nitintripathi774@gmail.com>
2026-09-13 00:15:30 +0530

    Time: 1 ms (48.02%), Space: 12 MB (14.2%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     string removeTrailingZeros(string num) {
-        while(num.back() == '0') num.pop_back();  return num;
-       
+       int i = num.length() -1;
+       while(i >= 0 && num[i] == '0') i--;
+       string ans = "";
+       for(int j = 0; j <= i; j++) ans += num[j];
+       return ans;
     }
 };
