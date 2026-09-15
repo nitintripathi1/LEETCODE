3505ac72be4dad9a9708a6c882c650503c9379f5
NITIN TRIPATHI <nitintripathi774@gmail.com>
2026-09-15 13:24:04 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    int solve(vector<int>& nums, int i, vector<int>& dp) {
+        if (i < 0)
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+        int loot = nums[i] + solve(nums, i - 2, dp);
+        int skip = solve(nums, i - 1, dp);
+        return dp[i] = max(loot, skip);
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1) return nums[0];
-        if(n == 2) return max(nums[0] , nums[1]);
-        vector<int>dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0] , nums[1]);
-        for(int i = 2; i < n; i++){
-            dp[i] = max(nums[i] + dp[i-2] , dp[i-1]);
-            
-        }
-        return dp[n-1];
+        vector<int> dp(n, -1);
+
+        return solve(nums, n - 1, dp);
     }
 };
