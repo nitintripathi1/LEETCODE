a4f5f09097088d99bcb929d140fe1087d6569e08
NITIN TRIPATHI <nitintripathi774@gmail.com>
2026-09-21 02:02:49 +0530

    Time: 0 ms (100%), Space: 11.2 MB (26.01%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        int count = 0;
-        while(true){
-        int min = INT_MAX;
+        unordered_set<int>s;
         for(int i = 0; i < nums.size(); i++){
-            if(nums[i] < min && nums[i] != 0){
-                min = nums[i];
-            }
-
+            if(nums[i] != 0) s.insert(nums[i]);
         }
-        if(min == INT_MAX) break;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] != 0)
-            nums[i] -= min; 
-        }
-        count++;
-        }
-        return count;
+        return s.size();
     }
 };
